30e60dd9d6b907da1fbdec346fa2459bce7e75d9
DeeprajGhadashi <deeprajghadshi1920@gmail.com>
2024-06-08 20:36:41 +0530

    AssertionTest 2) expect(page).toHaveTitle() -- page has title is correct or not

--- tests/Assertions.spec.js
+++ tests/Assertions.spec.js
@@ -5,6 +5,17 @@ test('AssertionTest', async({page})=>{
     // open App page
     await page.goto('https://demo.nopcommerce.com/register');
 
-    //1) expect (page).toHaveURL()  -- page has URL
+    //1) expect(page).toHaveURL()  -- page has URL is correct or not
     await expect(page).toHaveURL('https://demo.nopcommerce.com/register');
+
+    //2) expect(page).toHaveTitle() -- page has title is correct or not
+    await expect(page).toHaveTitle('nopCommerce demo store. Register');
+
+
+
+
+
+
+
+
 })
