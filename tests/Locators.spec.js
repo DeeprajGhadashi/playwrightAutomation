86d917a9ced01b4e2da24a081d62f372dafca414
DeeprajGhadashi <deeprajghadshi1920@gmail.com>
2024-06-08 17:32:07 +0530

    page login click on login + fill username + fill password + click on Login button

--- tests/Locators.spec.js
+++ tests/Locators.spec.js
@@ -4,7 +4,19 @@ import {test, expect} from '@playwright/test';
 test ('Locators', async ({page})=>{
     await page.goto("https://www.demoblaze.com/index.html");
 
-    //Click on login button -property
+    //Click on login button - used property
     //await page.locator('id=login2').click()    //approach 1
      await page.click('id=login2');     //approach 2
+
+     //provide usename  - used CSS
+    //await page.locator('#loginusername').fill("pavanol");
+     await page.fill('#loginusername','pavanol');
+     
+
+     //provide password - used CSS
+     await page.fill("input[id='loginpassword']",'test@123')
+
+     //Click on login button - used Xpath
+     await page.click("//button[normalize-space()='Log in']");
+
 })
