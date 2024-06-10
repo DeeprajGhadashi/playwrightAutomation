8b570b30bf78dd500056d709f3015c20b8e8cef3
DeeprajGhadashi <deeprajghadshi1920@gmail.com>
2024-06-10 23:34:10 +0530

    4) check presence of value in the dropdown - Approach 2 - using looping

--- tests/DropDowns.spec.js
+++ tests/DropDowns.spec.js
@@ -12,14 +12,37 @@ test('Handle dropdowns',async ({page})=> {
 
     //Assertions
     //1) check number of options in dropdown -- Approach 1
-    /*const options = await page.locator('#country option')
-      await expect(options).toHaveCount(9);
-    */
+    //const options = await page.locator('#country option')
+    //await expect(options).toHaveCount(9);
+    
 
     //2) check number of options in dropdown -- Approach 2
-    const options = await page.$$('#country option')
+    //const options = await page.$$('#country option')
     //console.log("Number of options:", options.length)
-    await expect(options.length).toBe(10);
+    //await expect(options.length).toBe(10);
+
+    //3) check presence of value in the dropdown - Approach 1
+    //const content = await page.locator('#country').textContent();
+    //await expect(content.includes('India')).toBeTruthy();
+
+    //4) check presence of value in the dropdown - Approach 2 - using looping
+    const options=await page.$$('#country option')
+    let status=false;
+    for(const option of options)
+    {
+     //console.log(await option.textContent())
+     
+     let value=await option.textContent();
+     if(value.includes('France'))
+     {
+       status=true;
+       break;
+     }
+    }
+    expect(status).toBeTruthy();
+
+
+    
 
 
 
