28827263d5b87d7bbb50dc4549a1301f8f2a1cc9
DeeprajGhadashi <deeprajghadshi1920@gmail.com>
2024-06-10 23:52:21 +0530

    Assertions 2) check number of options in dropdown using JS array

--- tests/MultiSelectDropdown.spec.js
+++ tests/MultiSelectDropdown.spec.js
@@ -9,8 +9,13 @@ test("Handle dropdowns",async ({page})=>{
 
     //Assertions
     //1) check number of options in dropdown
-    const options=await page.locator('#colors option')
-    await expect(options).toHaveCount(5);
+    //const options=await page.locator('#colors option')
+    //await expect(options).toHaveCount(5);
+
+    //2) check number of options in dropdown using JS array
+    //const options=await page.$$('#colors option')
+    //console.log("Number of options:",options.length)
+    //await expect(options.length).toBe(5);
 
 
      await page.waitForTimeout(5000);
