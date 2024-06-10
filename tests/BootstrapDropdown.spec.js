8e40d046739df0763ca482e03db1e12d2b11e707
DeeprajGhadashi <deeprajghadshi1920@gmail.com>
2024-06-11 00:30:51 +0530

    deselect options from dropdown

--- tests/BootstrapDropdown.spec.js
+++ tests/BootstrapDropdown.spec.js
@@ -13,18 +13,31 @@ test('Bootstrap dropdown', async ({page}) =>{
     //const options=await page.$$('ul>li label input')
     //await expect(options.length).toBe(11)
 
-   //3) select options from dropdown
-   const options=await page.$$('ul>li label')
+    //3 select options from dropdown
+    /*const options=await page.$$('ul>li label')
     for(let option of options)
     {
         const value=await option.textContent();
-        console.log("value is",value)
+        //console.log("value is",value)
         if(value.includes('Angular') || value.includes('Java'))
         {
             await option.click()
         }
 
-    }
+    }*/
+
+     //4) deselect options from dropdown
+     const options=await page.$$('ul>li label')
+     for(let option of options)
+     {
+         const value=await option.textContent();
+         console.log("value is",value)
+         if(value.includes('HTML') || value.includes('CSS'))
+         {
+             await option.click()
+         }
+ 
+     }
 
 
 
