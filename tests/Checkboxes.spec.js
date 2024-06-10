08f3deedbbd68ef0112f757415dc5ae6e5161848
DeeprajGhadashi <deeprajghadshi1920@gmail.com>
2024-06-10 20:20:52 +0530

    Multiple checkboxes ( select multiple days)

--- tests/Checkboxes.spec.js
+++ tests/Checkboxes.spec.js
@@ -7,9 +7,20 @@ test ('Handle checkboxes', async({page}) =>{
     await page.locator("//input[@id='monday' and @type='checkbox']").click();
     //await page.click("//input[@id='monday' and @type='checkbox']");
 
-    expect( await page.locator("//input[@id='monday' and @type='checkbox']")).toBeChecked();
-    expect( await page.locator("//input[@id='monday' and @type='checkbox']").isChecked()).toBeTruthy();
+    //expect( await page.locator("//input[@id='monday' and @type='checkbox']")).toBeChecked();
+    //expect( await page.locator("//input[@id='monday' and @type='checkbox']").isChecked()).toBeTruthy();
+    //expect( await page.locator("//input[@id='sunday' and @type='checkbox']").isChecked()).toBeFalsy();    //is not selected then test case is pass
 
+     //Multiple checkboxes
+     const checkboxeLocators = [ "//input[@id='monday' and @type='checkbox']",
+                          "//input[@id='sunday' and @type='checkbox']",
+                          "//input[@id='saturday' and @type='checkbox']"
+                        ];
+
+    for(const locator of checkboxeLocators )
+    {
+        await page.locator(locator).check();
+    }
 
     await page.waitForTimeout(5000);
 })
