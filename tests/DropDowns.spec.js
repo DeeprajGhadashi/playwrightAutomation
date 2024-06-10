b2e3626cdbf70f6a68632b99ee01dfdf21c3f56e
DeeprajGhadashi <deeprajghadshi1920@gmail.com>
2024-06-10 23:08:55 +0530

    1) check number of options in dropdown using  Assertions

--- tests/DropDowns.spec.js
+++ tests/DropDowns.spec.js
@@ -4,11 +4,16 @@ test('Handle dropdowns',async ({page})=> {
     await page.goto("https://testautomationpractice.blogspot.com/");
 
     //Multiple ways to select option from the dropdwon
-    await page.locator("#country").selectOption({label:'India'}); //label/ visible text
+    //await page.locator("#country").selectOption({label:'India'}); //label/ visible text
     //await page.locator("#country").selectOption('India');  //visible text
     //await page.locator("#country").selectOption({value: 'uk'});  //by using value attribute
     //await page.locator("#country").selectOption({index: 1});    // by using index
-    // await page.selectOption("#country",'India');  //by text
+    //await page.selectOption("#country",'India');  //by text
+
+    //Assertions
+    //1) check number of options in dropdown
+    const options = await page.locator('#country option')
+    await expect(options).toHaveCount(9);
 
 
     await page.waitForTimeout(2000);
