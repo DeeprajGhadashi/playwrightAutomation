806d11452e24511febe925ceac6cf1592ff64939
DeeprajGhadashi <deeprajghadshi1920@gmail.com>
2024-06-11 19:37:03 +0530

    Drag And Drop - Appraoch 3

--- tests/DragAndDrop.spec.js
+++ tests/DragAndDrop.spec.js
@@ -18,6 +18,11 @@ test ('Drag And Drop', async ({page})=>{
 //Appraoch 2
 await rome.dragTo(italy)
 
+//WASHINGTON---> US Appraoch 3
+const washington=await page.locator('#box3')
+const usa=await page.locator('#box103')
+
+await washington.dragTo(usa)
 
 await page.waitForTimeout(5000)
 
