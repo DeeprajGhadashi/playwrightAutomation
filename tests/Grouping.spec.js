27bf331cd81772d4242a845ef2841148c4255905
DeeprajGhadashi <deeprajghadshi1920@gmail.com>
2024-06-12 00:02:26 +0530

    multiple test individual perform one by one

---
+++ tests/Grouping.spec.js
@@ -0,0 +1,18 @@
+const { test, expect } = require('@playwright/test');
+
+test('Test1', async({page})=>{
+    console.log('this is test 1....')
+})
+
+test('Test2', async({page})=>{
+    console.log('this is test 2....')
+})
+
+test('Test3', async({page})=>{
+    console.log('this is test 3....')
+})
+
+
+test('Test4', async({page})=>{
+    console.log('this is test 4....')
+})
