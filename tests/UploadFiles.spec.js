78e365e5742686b4330b9083d287c4ffc400569d
DeeprajGhadashi <deeprajghadshi1920@gmail.com>
2024-06-11 20:48:21 +0530

    Multiple Files Upload at a time  test in playwright

--- tests/UploadFiles.spec.js
+++ tests/UploadFiles.spec.js
@@ -11,4 +11,19 @@ test('Single File',async ({page})=>{
     await page.locator('[name=upfile]').setInputFiles('tests/uploadFiles/msbte-logo.jpg')
     await page.locator('[value=Press]').click();
     await page.waitForTimeout(5000)
+});
+
+test.only('Multiple Files',async ({page})=>{
+
+    await page.goto('https://davidwalsh.name/demo/multiple-file-upload.php')
+
+    await page.locator('#filesToUpload')
+                .setInputFiles(['tests/uploadFiles/Deepraj_Ghadashi_7743832393-Resume.pdf',
+                                 'tests/uploadFiles/msbte-logo.jpg']);
+
+    await page.waitForTimeout(3000)
+    expect (await page.locator('#fileList li:nth-child(1)')).toHaveText('Deepraj_Ghadashi_7743832393-Resume.pdf')
+    expect (await page.locator('#fileList li:nth-child(2)')).toHaveText('msbte-logo.jpg')
+
+    await page.waitForTimeout(3000)
 })
