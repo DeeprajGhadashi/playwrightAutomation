56a82348e11cd7a3b05026b535c12f148621a786
DeeprajGhadashi <deeprajghadshi1920@gmail.com>
2024-06-11 17:21:58 +0530

    total number of rows & columns  -- PAGINATION TABLE

--- tests/Table.spec.js
+++ tests/Table.spec.js
@@ -4,16 +4,26 @@ test("handling table",async ({page})=>{
 
     await page.goto('https://testautomationpractice.blogspot.com/');
 
-    const table=await page.locator('[name=BookTable]')
+    //const table=await page.locator('[name=BookTable]')
+    const table=await page.locator('#productTable')
 
     // 1) total number of rows & columns  -- WEB TABLE 
-    const columns= await table.locator('tbody tr th')
+    /*const columns= await table.locator('tbody tr th')
     console.log('Number of columns:', await columns.count()) //4
     expect(await columns.count()).toBe(4)
 
     const rows=await table.locator('tbody tr ')
     console.log('Number of rows:', await rows.count()) //5
     expect(await rows.count()).toBe(7)
+   */
+  
+    // 2) total number of rows & columns  -- PAGINATION TABLE
+    const columns2= await table.locator('thead tr th')
+    console.log('Number of columns:', await columns2.count()) //4
+    expect(await columns2.count()).toBe(4)
 
+    const rows2 =await table.locator('tbody tr')
+    console.log('Number of rows:', await rows2.count()) //5
+    expect(await rows2.count()).toBe(5)
 
 })
