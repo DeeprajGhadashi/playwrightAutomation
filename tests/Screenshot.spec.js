38b9244b7566e4c88678ade5d3440d2bcacc74ae
DeeprajGhadashi <deeprajghadshi1920@gmail.com>
2024-06-12 00:25:00 +0530

    Full page screenshot

--- tests/Screenshot.spec.js
+++ tests/Screenshot.spec.js
@@ -3,7 +3,10 @@ import { test, expect } from '@playwright/test';
 test('page screenshot', async ({ page }) => {
  await page.goto('https://demo.opencart.com/')
  await page.screenshot({ path:'tests/screenshots/'+Date.now()+'HomePage.png'})
+ await page.waitForTimeout(5000);
 });
+
+ 
 
 test('Full page screenshot', async ({ page }) => {
     await page.goto('https://demo.opencart.com/')
