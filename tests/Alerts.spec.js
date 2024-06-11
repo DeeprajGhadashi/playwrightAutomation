c44be40ebe9abc958c6070c3f8e5fc1d1d106a29
DeeprajGhadashi <deeprajghadshi1920@gmail.com>
2024-06-11 12:36:07 +0530

    ALERT -Confirmation Dialod-Alert with OK and cancel

--- tests/Alerts.spec.js
+++ tests/Alerts.spec.js
@@ -1,6 +1,6 @@
 const { test, expect } = require('@playwright/test');
 
-test('Alert with OK', async ({ page }) => {
+test.skip('Alert with OK', async ({ page }) => {
   await page.goto('https://testautomationpractice.blogspot.com/');
 
   //Enabling Dilaog window alert handling
@@ -13,4 +13,22 @@ test('Alert with OK', async ({ page }) => {
 await page.click('//button[normalize-space()="Alert"]');
 await page.waitForTimeout(5000);
 
+});
+
+test('Confirmation Dialod-Alert with OK and cancel', async ({ page }) => {
+  await page.goto('https://testautomationpractice.blogspot.com/');
+
+  //Enabling Dilaog window alert handling
+   page.on('dialog', async dialog=> {
+   expect(dialog.type()).toContain('confirm')
+   expect(dialog.message()).toContain('Press a button!')
+   await dialog.accept(); // close by using OK button
+   //await dialog.dismiss(); // close by using cancel
 })
+
+  await page.click('//button[normalize-space()="Confirm Box"]');
+  await expect(page.locator('//p[@id="demo"]')).toHaveText('You pressed OK!')
+
+  await page.waitForTimeout(5000);
+
+});
