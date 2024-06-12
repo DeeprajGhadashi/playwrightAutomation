1760a2cdf4501b46a3eb6cf7e3f528cffe9f3553
DeeprajGhadashi <deeprajghadshi1920@gmail.com>
2024-06-12 16:26:03 +0530

    run all test without tag

---
+++ tests/Tags.spec.js
@@ -0,0 +1,24 @@
+const { test, expect } = require('@playwright/test')
+
+test('test1',async({page})=>{
+    console.log('this is my test1...')
+})
+
+
+test('test2',async({page})=>{
+    console.log('this is my test2...')
+})
+
+
+test('test3',async({page})=>{
+    console.log('this is my test3...')
+})
+
+
+test('test4',async({page})=>{
+    console.log('this is my test4...')
+})
+
+test('test5',async({page})=>{
+    console.log('this is my test5...')
+})
