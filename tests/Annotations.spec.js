d3a98a11b3a94bec4df16701dee57a836ebac932
DeeprajGhadashi <deeprajghadshi1920@gmail.com>
2024-06-12 17:40:01 +0530

    skip test approach 2

--- tests/Annotations.spec.js
+++ tests/Annotations.spec.js
@@ -5,8 +5,16 @@ test('test1', async({page})=>{
     console.log('this is test1')
 })
 
-//skip
+//skip approach 1
 test.skip('test2', async({page})=>{
     console.log('this is test2')
 })
 
+//skip approach 2
+test('test3', async({page, browserName})=>{
+    console.log('this is test3')
+    if(browserName==='chromium')
+    {
+        test.skip()
+    }
+})
