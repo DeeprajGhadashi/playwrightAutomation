2b4ee696813be0ea2676fd0da6945498cbe2f004
DeeprajGhadashi <deeprajghadshi1920@gmail.com>
2024-06-12 17:49:01 +0530

    fixme annotation used like skip any bug in your test then the skip using fixme

--- tests/Annotations.spec.js
+++ tests/Annotations.spec.js
@@ -1,10 +1,11 @@
 const {test, expect}=require('@playwright/test')
 
 // only
-test('test1', async({page})=>{
+test.only('test1', async({page})=>{
     console.log('this is test1')
 })
 
+/*
 //skip approach 1
 test.skip('test2', async({page})=>{
     console.log('this is test2')
@@ -13,8 +14,17 @@ test.skip('test2', async({page})=>{
 //skip approach 2
 test('test3', async({page, browserName})=>{
     console.log('this is test3')
-    if(browserName==='firefox')
+    if(browserName==='chromium')
     {
         test.skip()
     }
+})
+   
+*/
+
+//Fixme
+test('test4', async({page})=>{
+    test.fixme()
+    console.log('this is test 4..')
+
 })
