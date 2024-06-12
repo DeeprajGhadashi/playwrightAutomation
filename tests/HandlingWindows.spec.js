9d03a3cae7695e464c1556c7394d8934bdfc7b1a
DeeprajGhadashi <deeprajghadshi1920@gmail.com>
2024-06-12 22:22:26 +0530

    Handle Multiple Pages/Windows first open one page then navigate to other page click on link

--- tests/HandlingWindows.spec.js
+++ tests/HandlingWindows.spec.js
@@ -17,4 +17,26 @@ test('Handle Pages/Windows', async () => {
   await page2.goto("https://www.orangehrm.com/")
   await expect(page2).toHaveTitle("Human Resources Management Software | OrangeHRM")
 
+});
+
+test.only('Handle Multiple Pages/Windows', async () => {
+            
+  const browser=await chromium.launch()
+  const context=await browser.newContext()
+
+  const page1=await context.newPage()
+  await page1.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+  await expect(page1).toHaveTitle("OrangeHRM")
+
+  const pagePromise=context.waitForEvent('page')
+  await page1.locator('//a[normalize-space()="OrangeHRM, Inc"]').click()
+
+  const newPage= await pagePromise;
+  await expect(newPage).toHaveTitle("Human Resources Management Software | OrangeHRM")
+
+  await page1.waitForTimeout(3000)
+  await newPage.waitForTimeout(3000)
+  
+  await browser.close()
+
 })
