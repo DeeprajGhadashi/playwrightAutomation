b7d32544e28b7885c8ecf31349ba270a0a8a0838
DeeprajGhadashi <deeprajghadshi1920@gmail.com>
2024-06-12 20:13:35 +0530

    with pom- page object model pattern

--- tests/Pomtest.spec.js
+++ tests/Pomtest.spec.js
@@ -1,11 +1,38 @@
 import { test, expect } from '@playwright/test';
+import { LoginPage } from '../pages/LoginPage';
+import { HomePage } from '../pages/HomePage';
+import { CartPage } from '../pages/CartPage';
 
 test('test', async ({ page }) => {
 
     //-----  without pom- page object model pattern -----//
+  /*
   await page.goto('https://www.demoblaze.com/index.html');
   await page.locator('#login2').click();
   await page.locator('#loginusername').fill('pavanol');
   await page.locator('#loginpassword').fill('test@123');
   await page.locator('//button[normalize-space()="Log in"]').click();
+ */
+
+
+  //-----  with pom- page object model pattern -----//
+
+ //Login
+   const login = new LoginPage(page);
+   await login.gotoLoginPage();
+   await login.login('pavanol', 'test@123');
+   await page.waitForTimeout(3000);
+
+//Home
+   const home=new HomePage(page)
+   await home.addProductToCart("Nexus 6")
+   await page.waitForTimeout(3000)
+   await home.gotoCart();
+
+//Cart
+   const cart=new CartPage(page)
+   await page.waitForTimeout(3000)
+   const status=await cart.checkProductInCart('Nexus 6')
+   expect(await status).toBe(true);
+ 
 });
