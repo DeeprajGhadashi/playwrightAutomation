78b518265a523f2c985a9caef9c6af06ad61ef2a
DeeprajGhadashi <deeprajghadshi1920@gmail.com>
2024-06-12 16:18:23 +0530

    You can also use trace: 'retain-on-failure' if you do not enable retries but still want traces for failed tests.

--- tests/Tracing.spec.js
+++ tests/Tracing.spec.js
@@ -1,4 +1,15 @@
 import { test, expect } from '@playwright/test';
+/*
+Available options to record a trace:
+
+'on-first-retry' - Record a trace only when retrying a test for the first time.
+'on-all-retries' - Record traces for all test retries.
+'off' - Do not record a trace.
+'on' - Record a trace for each test. (not recommended as it's performance heavy)
+'retain-on-failure' - Record a trace for each test, but remove it from successful test runs.
+
+You can also use trace: 'retain-on-failure' if you do not enable retries but still want traces for failed tests.
+*/
 
 test('test', async ({ page }) => {
   await page.goto('https://www.demoblaze.com/index.html');
@@ -6,5 +17,5 @@ test('test', async ({ page }) => {
   await page.locator('#loginusername').fill('pavanol');
   await page.locator('#loginpassword').fill('test@123');
   await page.getByRole('button', { name: 'Log in' }).click();
-  await expect(page.locator('#logout2')).toBeVisible()
+  await expect(page.locator('#logout')).toBeVisible()
 });
