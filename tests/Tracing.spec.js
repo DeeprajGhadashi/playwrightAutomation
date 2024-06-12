87496be414642215b6b17684574c142e641bce43
DeeprajGhadashi <deeprajghadshi1920@gmail.com>
2024-06-12 16:19:42 +0530

    You can also use trace: 'retain-on-failure' if you do not enable retries but still want traces for failed tests.

--- tests/Tracing.spec.js
+++ tests/Tracing.spec.js
@@ -17,5 +17,5 @@ test('test', async ({ page }) => {
   await page.locator('#loginusername').fill('pavanol');
   await page.locator('#loginpassword').fill('test@123');
   await page.getByRole('button', { name: 'Log in' }).click();
-  await expect(page.locator('#logout')).toBeVisible()
+  await expect(page.locator('#logout')).toBeVisible()          // correct is '#logout2'
 });
