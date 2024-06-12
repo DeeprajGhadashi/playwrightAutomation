03437141a495f274b2daad2a84db886ab7bc951b
DeeprajGhadashi <deeprajghadshi1920@gmail.com>
2024-06-12 23:19:41 +0530

    all reporters generated

--- playwright.config.js
+++ playwright.config.js
@@ -23,7 +23,16 @@ module.exports = defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   //reporter: 'list',
   //reporter: [['json',{outputFile:'results.json'}]],
-    reporter: [['junit',{outputFile:'results.xml'}]],
+  //reporter: [['junit',{outputFile:'results.xml'}]],
+  reporter: [ ['list'],
+              ['html'],
+              ['line'],
+              ['dot'],
+              ['json',{outputFile:'results.json'}],
+              ['junit',{outputFile:'results.xml'}]
+            ],
+
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
