cbc2b5e8de23a77ca595ad9bcae946e6ce0a6a0b
DeeprajGhadashi <deeprajghadshi1920@gmail.com>
2024-06-13 15:15:34 +0530

    Create users using POST request

--- tests/APITests.spec.js
+++ tests/APITests.spec.js
@@ -1,7 +1,9 @@
 const {test, expect}=require('@playwright/test')
 
+var userid;
+
 //GET
-test.only('Get users', async ({request}) => {
+test('Get users', async ({request}) => {
 
    const response= await request.get('https://reqres.in/api/users?page=2')
    console.log( await response.json())
@@ -9,8 +11,24 @@ test.only('Get users', async ({request}) => {
 });
 
 //POST
-test('Create users', async ({request}) => {
+test.only('Create users', async ({request}) => {
 
+    const response= await request.post('https://reqres.in/api/users?page=2',
+        {
+            data:{
+                   "name" : "Deepraj Ghadashi",
+                   "job" : "Tester"
+            },
+            headers: {
+                   "Accept": "application/json"
+            }
+        });
+
+    console.log( await response.json())
+    expect(response.status()).toBe(201);
+
+    var res = await response.json()
+    userid = res.id
 });
 
 //PUT
